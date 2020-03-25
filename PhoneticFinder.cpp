3bfa6df882dac4a778bb160f5e149d011cb8eb9b
op1995 <pellerofir@gmail.com>
2020-03-25 22:00:27 +0200

    irrelevent update, testing bad-kan

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -5,7 +5,7 @@ using namespace std;
 namespace phonetic {
     string find(string text, string word){
     	if (word.size()==0) throw invalid_argument("Inserted word is of length 0!");
-        cout << "Grade: 98" << endl;
+        cout << "Grade: 56" << endl;
         return "happy";
     }
 }
